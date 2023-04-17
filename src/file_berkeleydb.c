e20eb2afd677eb18a43909d616601014e0ff25a3
Daniel Kokoszka <kokoszka.d@gmail.com>
2023-04-17 10:49:37 -0400

    consistent file ext, whitespace

--- src/file_berkeleydb.c
+++ src/file_berkeleydb.c
@@ -19,33 +19,33 @@ const file_hint_t file_hint_berkeleydb= {
   .max_filesize=PHOTOREC_MAX_FILE_SIZE,
   .recover=1,
   .enable_by_default=1,
-	.register_header_check=&register_header_check_berkeleydb
+  .register_header_check=&register_header_check_berkeleydb
 };
 
-struct __db_lsn { /* SHARED */
-	uint32_t	file;		/* File ID. */
-	uint32_t	offset;		/* File offset. */
+struct __db_lsn {  /* SHARED */
+  uint32_t file;   /* File ID. */
+  uint32_t offset; /* File offset. */
 };
 
 typedef struct __db_lsn DB_LSN;
 
 struct db_header {
-	DB_LSN	  lsn;		/* 00-07: LSN. */
-	uint32_t pgno;		/* 08-11: Current page number. */
-	uint32_t magic;	/* 12-15: Magic number. */
-	uint32_t version;	/* 16-19: Version. */
-	uint32_t pagesize;	/* 20-23: Pagesize. */
-	uint8_t  encrypt_alg;	/*    24: Encryption algorithm. */
-	uint8_t  type;		/*    25: Page type. */
-	uint8_t  metaflags;	/* 26: Meta-only flags */
-	uint8_t  unused1;	/* 27: Unused. */
-	uint32_t free;		/* 28-31: Free list page number. */
-	uint32_t last_pgno;	/* 32-35: Page number of last page in db. */
-	uint32_t nparts;	/* 36-39: Number of partitions. */
-	uint32_t key_count;	/* 40-43: Cached key count. */
-	uint32_t record_count;	/* 44-47: Cached record count. */
-	uint32_t flags;	/* 48-51: Flags: unique to each AM. */
-	uint8_t  uid[20]; /* 52-71: Unique file ID. */
+  DB_LSN   lsn;          /* 00-07: LSN. */
+  uint32_t pgno;         /* 08-11: Current page number. */
+  uint32_t magic;        /* 12-15: Magic number. */
+  uint32_t version;      /* 16-19: Version. */
+  uint32_t pagesize;     /* 20-23: Pagesize. */
+  uint8_t  encrypt_alg;  /*    24: Encryption algorithm. */
+  uint8_t  type;         /*    25: Page type. */
+  uint8_t  metaflags;    /*    26: Meta-only flags */
+  uint8_t  unused1;      /*    27: Unused. */
+  uint32_t free;         /* 28-31: Free list page number. */
+  uint32_t last_pgno;    /* 32-35: Page number of last page in db. */
+  uint32_t nparts;       /* 36-39: Number of partitions. */
+  uint32_t key_count;    /* 40-43: Cached key count. */
+  uint32_t record_count; /* 44-47: Cached record count. */
+  uint32_t flags;        /* 48-51: Flags: unique to each AM. */
+  uint8_t  uid[20];      /* 52-71: Unique file ID. */
 } __attribute__ ((gcc_struct, __packed__));
 
 /*@
@@ -60,7 +60,7 @@ static int header_check_berkeleydb(const unsigned char *buffer, const unsigned i
 
   reset_file_recovery(file_recovery_new);
 #ifdef DJGPP
-  file_recovery_new->extension="db";
+  file_recovery_new->extension="bdb";
 #else
   file_recovery_new->extension=file_hint_berkeleydb.extension;
 #endif
